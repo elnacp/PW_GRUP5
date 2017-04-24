0af4501b6b9d93a4a94401ee573dda5c756a03fc
Elna Cabot Paredes <ls30917@salleurl.edu>
2017-04-24 16:33:13 +0200

    Middleware incorporat

--- app/config/routes.php
+++ app/config/routes.php
@@ -1,9 +1,26 @@
 <?php
 
+use Silex\Application;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
+
+//SESSION
+$before = function (Request $request, Application $app){
+    if(!$app['session']->has('name')){
+        $response = new Response();
+        $content = $app['twig']->render('error.twig', [
+            'message' => 'You must be logged'
+        ]);
+        $response->setContent($content);
+        $response->setStatusCode(Response::HTTP_FORBIDDEN);
+        return $response;
+    }
+};
 
 
 $app->get('/', 'SilexApp\\Controller\\TaskController::indexAction');
-$app->get('/edit', 'SilexApp\\Controller\\TaskController::editProfile');
-
+//$app->get('/edit', 'SilexApp\\Controller\\TaskController::editProfile');
+$app->get('/test', 'SilexApp\\Controller\\BaseController::indexAction');
+$app->get('/admin', 'SilexApp\\Controller\\BaseController::adminAction')->before($before);
 
---
+++ src/Controller/BaseController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace  SilexApp\Controller;
+
+use Silex\Application;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class BaseController{
+    public function indexAction(Application $app){
+        if($app['session']->has('name')){
+            $app['session']->remove('name');
+            return new Response(('Session finished'));
+        }
+        $app['session']->set('name', 'Elna');
+        $content = 'Session started for the user' . $app['session']->get('name');
+        return new Response($content);
+    }
+    public function adminAction(Application $app){
+        $content = 'Welcome back' . $app['session']->get('name');
+        return new Response($content);
+    }
+}

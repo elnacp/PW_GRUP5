d386066b295dda4075b150e6d2df4143f5af7742
Elna Cabot Paredes <ls30917@salleurl.edu>
2017-05-01 22:18:45 +0200

    Plantilla de galeria editada

--- web/assets/js/galeria.js
+++ web/assets/js/galeria.js
@@ -1,16 +1,26 @@
 $(document).ready(function(){
 
-    $(".fancybox").fancybox({
-        openEffect: "none",
-        closeEffect: "none",
-        padding:0, margin:0,
+    $(".filter-button").click(function(){
+        var value = $(this).attr('data-filter');
 
-        beforeShow: function () {
-            $("body *:not(.fancybox-overlay, .fancybox-overlay *)").addClass("blur");
-        },
-        afterClose: function () {
-            $("body *:not(.fancybox-overlay, .fancybox-overlay *)").removeClass("blur");
+        if(value == "all")
+        {
+            //$('.filter').removeClass('hidden');
+            $('.filter').show('1000');
         }
+        else
+        {
+//            $('.filter[filter-item="'+value+'"]').removeClass('hidden');
+//            $(".filter").not('.filter[filter-item="'+value+'"]').addClass('hidden');
+            $(".filter").not('.'+value).hide('3000');
+            $('.filter').filter('.'+value).show('3000');
 
+        }
     });
+
+    if ($(".filter-button").removeClass("active")) {
+        $(this).removeClass("active");
+    }
+    $(this).addClass("active");
+
 });

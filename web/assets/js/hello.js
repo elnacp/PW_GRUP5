6a92d6eda2007e8eb5ee1d29d93f11a5a3dde8e3
Elna Cabot Paredes <ls30917@salleurl.edu>
2017-05-17 00:29:14 +0200

    Ajax ja casi funciona només falta carregar els 5 comentaris següents

--- web/assets/js/hello.js
+++ web/assets/js/hello.js
@@ -29,12 +29,13 @@ $(function () {
 });
 
 
+
+
 window.onload = function() {
 
     /*var params = {
-        'logejat': $('.logejat').attr('id')
-    };*/
-
+     'logejat': $('.logejat').attr('id')
+     };*/
 
 
     //console.log( $('.image_id').attr('id'));
@@ -50,11 +51,9 @@ window.onload = function() {
             console.log(response);
             totsPost = response;
             var logejat = $('.logejat').attr('id');
-            console.log("logejat" +logejat);
+            console.log("logejat" + logejat);
 
             //var div = document.getElementById('comentaris');
-
-
 
 
             for (var i = 0; i < totsPost['info'].length && i < 5; i++) {
@@ -68,33 +67,33 @@ window.onload = function() {
                 var h2 = document.createElement('h2');
                 var a1 = document.createElement('a');
                 var href2 = "/visualitzacioImatge/" + postVisibles[index]['img_id'];
-                a1.setAttribute("href", href2 );
+                a1.setAttribute("href", href2);
                 a1.innerHTML = postVisibles[index]['titol'];
                 h2.appendChild(a1);
                 div3.appendChild(h2);
                 var h3 = document.createElement('h3');
                 var a2 = document.createElement('a');
                 var href3 = "/perfil/" + postVisibles[index]['autor'];
-                a2.setAttribute("href", href3 );
+                a2.setAttribute("href", href3);
                 a2.innerHTML = postVisibles[index]['autor'];
                 h3.appendChild(a2);
                 div3.appendChild(h3);
                 var h5 = document.createElement('h5');
                 var span = document.createElement('span');
-                span.innerHTML = "Publicat - " + postVisibles[index]['publicat'] ;
+                span.innerHTML = "Publicat - " + postVisibles[index]['publicat'];
                 h5.appendChild(span);
                 div3.appendChild(h5);
                 var img = document.createElement('img');
-                img.setAttribute("class", "img-circle" );
-                img.setAttribute("src",postVisibles[index]['img_perfil']);
+                img.setAttribute("class", "img-circle");
+                img.setAttribute("src", postVisibles[index]['img_perfil']);
                 img.setAttribute("alt", "User Image");
                 div3.appendChild(img);
 
 
                 var img_post = document.createElement('img');
-                img_post.setAttribute("class", "img-thumbnail img-responsive center-block" );
+                img_post.setAttribute("class", "img-thumbnail img-responsive center-block");
                 img_post.setAttribute("id", "imgPost");
-                img_post.setAttribute("src",postVisibles[index]['img_path']);
+                img_post.setAttribute("src", postVisibles[index]['img_path']);
                 img_post.setAttribute("alt", "User Image");
 
                 div2.appendChild(div3);
@@ -105,69 +104,55 @@ window.onload = function() {
                 div4.setAttribute("class", "panel-footer");
 
                 var a = document.createElement('a');
-                if(logejat == 1) {
-                    var href4 = "/likeHome/" + postVisibles[index]['img_id'] + "/"+postVisibles[index]['autor'];
+                if (logejat == 1) {
+                    var href4 = "/likeHome/" + postVisibles[index]['img_id'] + "/" + postVisibles[index]['autor'];
                     a.setAttribute("href", href4);
                     //<a  href=" . $href1 . " class=\"[ btn btn-default ]\">Likes: +" . $likes . "</a>"
 
                 }
 
                 a.setAttribute("class", "[ btn btn-default ]");
-                a.innerHTML = "Likes: + " +postVisibles[index]['likes'];
+                a.innerHTML = "Likes: + " + postVisibles[index]['likes'];
                 div4.appendChild(a);
                 var button = document.createElement('button');
-                button.innerHTML = "Visitas: +" +postVisibles[index]['visitas'];
-                button.setAttribute("class","[ btn btn-default ]" );
+                button.innerHTML = "Visitas: +" + postVisibles[index]['visitas'];
+                button.setAttribute("class", "[ btn btn-default ]");
                 button.setAttribute("type", "button");
                 div4.appendChild(button);
                 //<div class=\"input-placeholder\">Escribe un comentario...</div>
-                var divaux = document.createElement('div');
-                divaux.setAttribute("class", "input-placeholder");
-                divaux.innerHTML = "Escribe un comentario...";
-                div4.appendChild(divaux);
+
                 div2.appendChild(div4);
 
-                if(logejat == 1) {
-                    console.log("ENTRA");
-
-                    var div5 = document.createElement('div');
-                    div5.setAttribute("class", "panel-google-plus-comment");
-                    var div6 = document.createElement('div');
-                    div6.setAttribute("class", "panel-google-plus-textarea");
-                    var form = document.createElement('form');
+                var div6 = document.createElement('div');
+                div6.setAttribute("class", "panel-google-plus-textarea");
+                var form = document.createElement('form');
+                if (logejat == 1) {
                     var href = "/comentari/" + postVisibles[index]['img_id'] + "/" + postVisibles[index]['autor'];
                     form.setAttribute("action", href);
-                    form.setAttribute("method", "POST");
-                    var textarea = document.createElement('textarea');
-                    textarea.setAttribute("rows", 4);
-                    textarea.setAttribute("name", "comentari");
-                    form.appendChild(textarea);
-                    var button = document.createElement('button');
-                    button.innerHTML = "Comentar";
-                    button.setAttribute("class", "[ btn btn-success disabled ]");
-                    button.setAttribute("type", "button");
-                    form.appendChild(button);
-                    div6.appendChild(form);
-                    var button = document.createElement('button');
-                    button.innerHTML = "Cancelar";
-                    button.setAttribute("class", "[ btn btn-default ]");
-                    button.setAttribute("type", "reset");
-                    div6.appendChild(button);
-                    div5.appendChild(div6);
-                    var div7 = document.createElement('div');
-                    div7.setAttribute("class", "clearfix");
-                    div5.appendChild(div7);
-                    div2.appendChild(div5);
-
                 }
+                form.setAttribute("method", "POST");
+                var textarea = document.createElement('textarea');
+                textarea.setAttribute("rows", 4);
+                textarea.setAttribute("name", "comentari");
+                textarea.setAttribute("class", "center-block");
+                textarea.setAttribute("id", "comentariArea");
+                var br = document.createElement('br');
+                var button = document.createElement('button');
+                button.innerHTML = "Comentar";
+                button.setAttribute("class", "comentariButton center-block");
+                button.setAttribute("type", "submit");
 
+                form.appendChild(textarea);
+                form.appendChild(br);
+                form.appendChild(button);
+                div6.appendChild(form);
+                var div7 = document.createElement('div');
+                div7.setAttribute("class", "clearfix");
+                div6.appendChild(div7);
+                div2.appendChild(div6);
 
 
                 $(div2).insertBefore("#loadmore");
-
-
-
-
 
 
                 index++;
@@ -176,16 +161,16 @@ window.onload = function() {
         }
     });
 
-    $('#loadmore').click(function(){
+    $('#loadmore').click(function () {
         console.log('hola');
         console.log(index);
 
-        if( totsPost.length == postVisibles.length && postVisibles.length != 0){
+        if (totsPost.length == postVisibles.length && postVisibles.length != 0) {
             alert('Tots carregats');
             return;
-        }else{
+        } else {
             //var div = document.getElementById('contenidorComentaris');
-            for( var i = 0; i < 5 && index < totsPost.length; i++){
+            for (var i = 0; i < 5 && index < totsPost.length; i++) {
 
 
             }
@@ -193,43 +178,4 @@ window.onload = function() {
         }
     });
 
-
-};
-
-/*console.log("hello");
- var div2 = document.createElement('div');
- div2.setAttribute("class", "[ panel panel-default ] panel-google-plus");
- var div3 = document.createElement('div');
- div3.setAttribute("class", "panel-heading");
- var h2 = document.createElement('h2');
- var href2 = "/visualitzacioImatge/" + postVisibles[index]['img_id'];
- h2.setAttribute("href", href2 );
- h2.innerHTML = postVisibles[index]['titol'];
- div3.appendChild(h2);
- var h3 = document.createElement('h3');
- var href3 = "/perfil/" + postVisibles[index]['autor'];
- h3.setAttribute("href", href3 );
- div3.appendChild(h3);
- var h5 = document.createElement('h5');
- var span = document.createElement('span');
- span.innerHTML = "Publicat" .postVisibles[index]['publicat'] ;
- h5.appendChild(span);
- div3.appendChild(h5);
- var img = document.createElement('img');
- img.setAttribute("class", "img-circle" );
- img.setAttribute("src",postVisibles[index]['img_perfil']);
- div3.appendChild(img);
- div2.appendChild(div3);
-
- var img_post = document.createElement('img');
- img_post.setAttribute("class", "img-thumbnail img-responsive center-block" );
- img_post.setAttribute("id", "imgPost");
- img.setAttribute("src",postVisibles[index]['img_path']);
- div2.appendChild(img_post);
-
- div.appendChild(div2);*/
-
-/*if(logejat == true){
- var a = document.createElement('a');
- var href4 = "/likeHome/" + postVisibles[index]['img_id'] +  "/" . postVisibles[index]['autor'];
- }*/
+}
